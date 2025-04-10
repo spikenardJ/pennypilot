67a426c091332b77034fbcc725f17d67d7d3350e
spikenardJ <168881170+spikenardJ@users.noreply.github.com>
2025-04-09 21:55:26 -0500

    Added firebaseConfig

---
+++ src/types/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtX1BljRuKDJ4n8Sm5ISt8LGDAZBCXO9c",
+    authDomain: "pennypilot-ac787.firebaseapp.com",
+    projectId: "pennypilot-ac787",
+    storageBucket: "pennypilot-ac787.firebasestorage.app",
+    messagingSenderId: "956945100876",
+    appId: "1:956945100876:web:8cf2f689a856c0e345c4e9"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
