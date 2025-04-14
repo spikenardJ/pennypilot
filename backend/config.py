28db9df66b29cb2e443fc996c3cc411195103a86
JaycobHoffman1 <jaycobhoffman2004@gmail.com>
2025-04-14 16:58:26 -0700

    fixed password and gitignore errors

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from root_password import root_password
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Jajoconi1@localhost/pennypilot_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://root:{root_password}@localhost/pennypilot_db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
