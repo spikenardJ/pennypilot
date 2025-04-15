3a17d26eb58eca02e6cb604543dac8550717e6dd
alex <alexalarcon200@gmail.com>
2025-04-15 16:03:35 -0400

    secured firebase credentials that were hardcoded

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 // import { AuthProvider } from "./context/auth";
-import React from "react";
-import Navbar from "./components/Navbar";
-import HomeDashboard from "./components/HomeDashboard";
-import BudgetPlanner from "./components/BudgetPlanner";
-import TaxPrep from "./components/TaxPrep";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import React from "react";
+// import Navbar from "./components/Navbar";
+// import HomeDashboard from "./components/HomeDashboard";
+// import BudgetPlanner from "./components/BudgetPlanner";
+// import TaxPrep from "./components/TaxPrep";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      {/* <AuthProvider> */}
-        <Routes>
-          <Route path="/" element={<HomeDashboard />} />
-          <Route path="/budget" element={<BudgetPlanner />} />
-          <Route path="/tax-prep" element={<TaxPrep />} />
-        </Routes>
-      {/* </AuthProvider> */}
-    </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <AuthProvider>
+//         <Routes>
+//           <Route path="/" element={<HomeDashboard />} />
+//           <Route path="/budget" element={<BudgetPlanner />} />
+//           <Route path="/tax-prep" element={<TaxPrep />} />
+//         </Routes>
+//       </AuthProvider>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
