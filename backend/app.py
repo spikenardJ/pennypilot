f42e5453e34de3d5b0bdc442d730dd8973a2ae45
JaycobHoffman1 <jaycobhoffman2004@gmail.com>
2025-04-16 18:23:54 -0700

    add tax info entity

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from routes.user import setup_user_routes
 from routes.linked_account import setup_linked_account_routes
 from routes.transaction import setup_transaction_routes
 from routes.goal import setup_goal_routes
+from routes.tax_info import setup_tax_info_routes
 from routes.home import setup_home_route
 
 setup_user_routes(app)
@@ -10,6 +11,7 @@ setup_linked_account_routes(app)
 setup_transaction_routes(app)
 setup_goal_routes(app)
 setup_home_route(app)
+setup_tax_info_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
